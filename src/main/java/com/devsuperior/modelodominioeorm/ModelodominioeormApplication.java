c5d41d0e79384ecba24071bfed2241f614d2dfad
wanderleySilva-silva <jwps.17@hotmail.com>
2025-01-29 15:06:48 -0300

    feat: Criadas as entidades e feitos relacionamentos

---
+++ src/main/java/com/devsuperior/modelodominioeorm/ModelodominioeormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modelodominioeorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelodominioeormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelodominioeormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/modelodominioeorm/ModelodominioeormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.modelodominioeorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelodominioeormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
